a7f00c49d00874816727a412cff428d852830d5f
Christian Abuto <abutochris2015@gmail.com>
2024-03-26 20:50:58 +0300

    update a file

--- react-conversion-rate/src/App.js
+++ react-conversion-rate/src/App.js
@@ -16,7 +16,7 @@ function App() {
     setLoading(true);
     setError('');
     try {
-      const response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/{endpoint}`);
+      const response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurrency}`);
       const data = await response.text(); // Get the response as text
       const jsonData = JSON.parse(data); // Parse the response as JSON
       const rate = jsonData[fromCurrency.toLowerCase()][toCurrency.toLowerCase()];
